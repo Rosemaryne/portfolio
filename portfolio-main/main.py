dae4d6ae0c50f8fcf6c0a2b9848b10ac3dd3e184
Rosemaryne <139486123+Rosemaryne@users.noreply.github.com>
2023-11-01 21:23:16 +0200

    Add files via upload

---
+++ portfolio-main/main.py
@@ -0,0 +1,43 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+#Подключение библиотеки баз данных
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+#Подключение SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Создание db
+db = SQLAlchemy(app)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+    return render_template('index.html', button_python=button_python, button_discord=button_discord, button_html=button_html,button_db=button_db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    id = db.Column(db.Integer, primary_key=True)
+    #Заголовок
+    title = db.Column(db.String(100), nullable=False)
+    #Описание
+    subtitle = db.Column(db.String(300), nullable=False)
+    #Текст
+    text = db.Column(db.Text, nullable=False)
+
+    #Вывод объекта и id
+    def __repr__(self):
+        return f'<Card {self.id}>'
